2990f069ea66a1ebfd5ece6944ab453e4dc293fd
itgitman <9078836@qq.com>
2022-06-11 11:14:18 +0800

    Initial commit

---
+++ src/main/java/com/example/springclouddemo/controller/Hello.java
@@ -0,0 +1,18 @@
+package com.example.springclouddemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author lenovo
+ * @Description:
+ * @Date created at 11:12 2022/6/11
+ * @Modified by lenovo
+ **/
+@RestController
+public class Hello {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello, world!";
+    }
+}
